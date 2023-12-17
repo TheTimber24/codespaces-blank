d5ed45940e7dec77b24171e785a1804e22a5fc70
Saturnman05 <112530281+Saturnman05@users.noreply.github.com>
2023-12-17 02:59:14 +0000

    first commit

---
+++ labestructura/camiones.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int i = 0;
+    std::cout << "Escribe un numero: ";
+    std::cin >> i;
+    std::cout << "El numero que escribiste fue el " << i << "\n";
+    return 0;
+}
