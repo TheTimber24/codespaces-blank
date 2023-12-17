49e0789a10a94265740fba3cbdd17f8a14875065
Saturnman05 <112530281+Saturnman05@users.noreply.github.com>
2023-12-17 03:04:27 +0000

    segundo commit

--- labestructura/camiones.cpp
+++ labestructura/camiones.cpp
@@ -4,6 +4,6 @@ int main() {
     int i = 0;
     std::cout << "Escribe un numero: ";
     std::cin >> i;
-    std::cout << "El numero que escribiste fue el " << i << "\n";
+    std::cout << "El numero que escribiste fue el " << i << "\n" << "Ese numero mas 2 es igual a " << (i + 2) << "\n";
     return 0;
 }
